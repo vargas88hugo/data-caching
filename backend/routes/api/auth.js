ecf72779a08a11c354fc14a69c8866b2c1ab2bf3
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-22 15:53:58 -0500

    [Update] Configured get user auth with jwt

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
+const auth = require('../../middlewares/auth');
+const User = require('../../models/User');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Auth Route');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
